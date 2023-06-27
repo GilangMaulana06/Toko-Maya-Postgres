656555e2797bc51fbb26876bbde5016d85f57887
GilangMaulana06 <gilangmaulana20061998@gmail.com>
2023-06-27 08:35:01 +0700

    up[date

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use('/', (req, res) => {
-    res.send('HELLO WORLD')
-})
 app.use('/api', routes)
 
 app.listen(3000, () => {
