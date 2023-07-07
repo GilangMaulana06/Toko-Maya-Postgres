b46802a8f4bed209fb1925d8bca8eec537d679bc
GilangMaulana06 <gilangmaulana20061998@gmail.com>
2023-07-07 20:58:24 +0700

    AA

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+const pool = require('./database')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -10,7 +11,17 @@ app.use(bodyParser.json())
 //     res.send('HELLO WORLD')
 // })
 
-app.use('/api', routes)
+// app.use('/api', routes)
+app.use('/api', (req, res) => {
+    if(req.method === 'GET'){
+        res.json([
+            {nama : 'Gilang', umur : '25'},
+            {nama : 'haikal', umur : '15'},
+        ])
+    } else {
+        res.send({message: 'INI POST'})
+    }
+})
 
 app.listen(3000, () => {
     console.log('listening on port:3000');
