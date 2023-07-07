e620272cc7558e05de55b738e113a89d16f91661
GilangMaulana06 <gilangmaulana20061998@gmail.com>
2023-07-07 20:16:57 +0700

    AA

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -2,14 +2,15 @@ const pool = require('../database')
 
 const getData = async (req, res) => {
     console.log('GET DATA')
-    try {
-        const data = await pool.query(
-            `SELECT * FROM data`
-        )
-        res.json(data.rows).status(200)
-    } catch (err) {
-        res.status(400).send(err.message)
-    }
+    // try {
+    //     const data = await pool.query(
+    //         `SELECT * FROM data`
+    //     )
+    //     res.json(data.rows).status(200)
+    // } catch (err) {
+    //     res.status(400).send(err.message)
+    // }
+    res.json({message: 'INI PESAN'})
 }
 
 const insertData = async (req, res) => {
